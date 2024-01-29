2ac26effdeba12d44d1a4acb76358c29aeaacf0e
Gabor <tarrga88@gmail.com>
2024-01-29 21:10:52 +0100

    style: nav

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -3,9 +3,11 @@ import { useSelector } from 'react-redux';
 
 export default function Navigation() {
   return (
-    <nav className="container">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
+    <nav>
+      <div className='wrapper'>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/about'>About</NavLink>
+      </div>
     </nav>
   );
 }
